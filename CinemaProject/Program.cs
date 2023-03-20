25ece0ff621bb995341ba78e1defab7e7603875d
ahmetozdemr <ahmetzdmiir@gmail.com>
2023-03-20 03:52:31 +0300

    Updated some methods

--- CinemaProject/Program.cs
+++ CinemaProject/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading;
 using CinemaProject.Data;
 using CinemaProject.Other;
@@ -10,11 +12,13 @@ namespace CinemaProject
     {
         static void Main(string[] args)
         {
-         //   ShowSimplePercentage();
+            //   ShowSimplePercentage();
+
             TheVisionFilms();
 
 
 
+            //   ShowSimplePercentage();
 
 
             Console.ReadKey();
@@ -42,24 +46,24 @@ namespace CinemaProject
             Console.WriteLine();
             Console.ResetColor();
         }
-       
+
         static void TheVisionFilms()
         {
-            StarShow(47);
+            StarShow(48);
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("                 VİZYONDAKİ FİLMLER");
             //  Console.ResetColor();
 
-            StarShow(47);
+            StarShow(48);
 
             Films films = new Films();
             Categories categories = new Categories();
             int t = 0;
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("{5} {0}{2}{1}{4}{3}", "|FİLM ADI|", "|FİYAT|", "             ", "|KATEGORİ|", "       ", "|NO|");
+            Console.WriteLine("{5} {0}{2}{1}{4}{3}", "|FİLM ADI|", "|FİYAT|", "         ", "|KATEGORİ|", "       ", "|NO|");
             Console.ResetColor();
-            StarShow(47);
+            StarShow(48);
             for (int i = 0; i < categories.categoryList.Count; i++)
             {
 
@@ -69,7 +73,7 @@ namespace CinemaProject
                 {
                     int lngth2;
                     string ls;
-                    if (t<9)
+                    if (t < 9)
                     {
                         lngth2 = (20 - films.filmList[t].Name.Length);
                         int l = 0;
@@ -93,12 +97,12 @@ namespace CinemaProject
                             l++;
                         }
                     }
-                    
-                    Console.WriteLine("{6}{0} {1}{3}{2}{4}{5}", "->", films.filmList[t].Name, films.filmList[t].Price + " TL", ls, "         ", categories.categoryList[i].Name,t+1);
+
+                    Console.WriteLine(" {6}{0} {1}{3}{2}{4}{5}", "->", films.filmList[t].Name, films.filmList[t].Price + " TL", ls, "         ", categories.categoryList[i].Name, t + 1);
                     t++;
                 }
 
-                StarShow(47);
+                StarShow(48);
 
             }
             Console.WriteLine();
@@ -107,13 +111,13 @@ namespace CinemaProject
             Console.WriteLine("Bir Kategori Seçiniz");
             for (int ii = 0; ii < categories.categoryList.Count; ii++)
             {
-                Console.WriteLine("       {0} => {1} ", ii, categories.categoryList[ii].Name);
+                Console.WriteLine("\t{0} => {1} ", ii, categories.categoryList[ii].Name);
             }
             byte chooseCategoryNumber = byte.Parse(Console.ReadLine());
             Console.ResetColor();
             Console.Clear();
             showTheFilmlistCategory(chooseCategoryNumber);
-     
+
 
             //ShowTheSeatNumber(chooseCategoryNumber);
         }
@@ -122,15 +126,15 @@ namespace CinemaProject
         {
             Films films = new Films();
             Categories categories = new Categories();
-            
-            StarShow(47);
+
+            StarShow(48);
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("{5}*{0}*{2}*{1}*{4}*{3}*", "FİLM ADI", "FİYAT", "             ", "KATEGORİ", "       ","NO");
-            StarShow(47);
+            Console.WriteLine("{5} {0}{2}{1}{4}{3}", "|FİLM ADI|", "|FİYAT|", "         ", "|KATEGORİ|", "       ", "|NO|");
+            StarShow(48);
             Console.ResetColor();
 
             int i = 0;
-            while (i<films.filmList.Count)
+            while (i < films.filmList.Count)
             {
                 if (films.filmList[i].CategoryId == categories.categoryList[chooseCategoryNumber].Id)
                 {
@@ -161,18 +165,25 @@ namespace CinemaProject
                             l++;
                         }
                     }
-                    
-                    Console.WriteLine("{6}{0} {1}{3}{2}{4}{5}", "->", films.filmList[i].Name, films.filmList[i].Price + " TL", ls, "         ", categories.categoryList[chooseCategoryNumber].Name, i + 1);
+
+                    Console.WriteLine(" {6}{0} {1}{3}{2}{4}{5}", "->", films.filmList[i].Name, films.filmList[i].Price + " TL", ls, "         ", categories.categoryList[chooseCategoryNumber].Name, i + 1);
                 }
                 i++;
             }
-            StarShow(47);
+            StarShow(48);
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("İzlemek istediğiniz Filmin No suna giriniz:");
+
+            byte chooseFilmNumber = byte.Parse(Console.ReadLine());
+            Console.ResetColor();
+            Console.Clear();
+            ShowTheSeatNumber(chooseFilmNumber);
         }
 
-        static void ShowTheSeatNumber(byte chooseCategory)
+        static void ShowTheSeatNumber(byte chooseFilmNumber)
         {
-         
-          
+
             Seats seats = new Seats();
             int t = 0;
             for (int i = 0; i < 3; i++)
@@ -189,6 +200,64 @@ namespace CinemaProject
             }
             StarShow(58);
             Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Bir koltuk seçiniz :");
+
+            byte chooseSeatNumber = byte.Parse(Console.ReadLine());
+            Console.ResetColor();
+            Console.Clear();
+
+            ShowTheTicket(chooseSeatNumber, chooseFilmNumber);
+
+        }
+
+        static void ShowTheTicket(byte chooseSeatNumber, byte chooseFilmNumber)
+        {
+            Films films = new Films();
+            Seats seats = new Seats();
+            Ticket ticket = new Ticket();   
+            Console.ResetColor();
+            StarShow(45);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Lütfen Adınızı Giriniz :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Lütfen Soyadınız Giriniz :");
+            string surName = Console.ReadLine();
+
+            ticket.Name = name;
+            ticket.SurName = surName;
+            ticket.FilmName = films.filmList[chooseFilmNumber - 1].Name;
+            ticket.FilmPrice = films.filmList[chooseFilmNumber - 1].Price;
+            ticket.DateTime = DateTime.Now;
+            
+
+            List<Ticket> ticketList = new List<Ticket>();
+            ticketList.Add(ticket);
+
+            Console.Clear();
+            StarShow(30);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine("\t====CİNEMA BİLETİ====");
+            Console.WriteLine("\tAd :{0}", ticket.Name);
+            Console.WriteLine("\tSoyad :{0}", ticket.SurName);
+            Console.WriteLine("\tFilm Adı :{0}", ticket.FilmName);
+            Console.WriteLine("\tKoltuk Numarası:{0}", seats.seatList[chooseSeatNumber - 1].Id);
+            Console.WriteLine("\tBilet Fİyatı:{0}", ticket.FilmPrice);
+            Console.WriteLine("\tAlınma Tarihi:{0}", ticket.DateTime);
+        Console.ResetColor();
+            StarShow(30);
+
+            Console.WriteLine("Yeni bilet almak için 1 sayısını tuşlayınız");
+            Console.WriteLine("Ya da herhangi bir rakam basınız");
+            byte click = byte.Parse(Console.ReadLine());
+            Console.Clear();
+            if (click == 1)
+            {
+                TheVisionFilms();
+            }
+
         }
 
     }
--- CinemaProject/Ticket.cs
+++ CinemaProject/Ticket.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CinemaProject.Entity;
 using CinemaProject.Other;
 
 namespace CinemaProject
 {
-    public class Ticket
+    public class Ticket:Customer
     {
         public int Id { get; set; } 
-        public Film FilmName { get; set; }  
-        public  Film FilmPrice { get; set; }    
+        public string FilmName { get; set; }  
+        public  double FilmPrice { get; set; }    
         public DateTime DateTime { get; set;}
     }
 }
