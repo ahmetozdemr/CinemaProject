da604d7f9ba08c587895206bc20658a9074f1ba3
ahmetozdemr <ahmetzdmiir@gmail.com>
2023-03-26 08:06:16 +0300

    Refactoring

--- CinemaProject/Business/TicketRegister.cs
+++ CinemaProject/Business/TicketRegister.cs
@@ -9,88 +9,14 @@ namespace CinemaProject.Business
 {
     public class TicketRegister : IOperation
     {
-        public void Add(byte selectedSeatNumber, byte selectedFilmNumber, Films films, Seats seats, Ticket ticket, Tickets tickets, ref int ticketId,int activeUserId, UserData userData)
+        public void Add(byte selectedSeatNumber, byte selectedFilmNumber, Films films, Seats seats, Ticket ticket, Tickets tickets, ref int ticketId, int activeUserId, UserData userData)
         {
-            /*
-            string firstName = null;
-            string lastName = null;
-            bool checkGap = true;
-            bool checkWrong = true;
-            while (checkGap)
-            {
-                try
-                {
-                    Console.ResetColor();
-                    Stars.Show(45);
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Lütfen Adınızı Giriniz :");
-                    firstName = Console.ReadLine();
-                    Console.WriteLine("Lütfen Soyadınız Giriniz :");
-                    lastName = Console.ReadLine();
-
-                    //Bu kısımda name ve surName değişkenlerinin boş,null ya da sadece boşluktan oluşup oluşmadığını kontrol ediyor.
-                    //Tüm kuralları sağlıyorsa while döngüsünden çıkıyor
-                    //Burdaki y değişkeni if ten sonra bir hata oluşursa hata çıktısı verirsin diye konuldu
-                    if (!String.IsNullOrEmpty(firstName) && !String.IsNullOrEmpty(lastName))
-                    {
-                        foreach (var item1 in firstName)
-                        {
-                            if (item1 != ' ')
-                            {
-                                foreach (var item2 in lastName)
-                                {
-                                    if (item2 != ' ')
-                                    {
-                                        checkGap = !checkGap;
-                                        checkWrong = true;
-                                    }
-                                }
-                                checkWrong = !checkWrong;
-                            }
-                            checkWrong = !checkWrong;
-                        }
-                        checkWrong = !checkWrong;
-                    }
-                    else
-                    {
-                        checkGap = true;
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Lütfen gerekli yerleri doldurunuz");
-                        Console.ResetColor();
-                    }
-                    if (!checkWrong)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Lütfen gerekli yerleri doldurunuz");
-                        Console.ResetColor();
-                    }
-
-                }
-                catch
-                {
-
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Lütfen geçerli bir işlem tuşlayınız !!!");
-                    Console.ResetColor();
-                    checkGap = true;
-                }
-            }
-            */
             ticket.Id = userData.UserDataGet()[activeUserId - 1].Id;
-            ticket.FirstName = userData.UserDataGet()[activeUserId-1].FirstName;
+            ticket.FirstName = userData.UserDataGet()[activeUserId - 1].FirstName;
             ticket.LastName = userData.UserDataGet()[activeUserId - 1].LastName;
             ticket.FilmName = films.filmList[selectedFilmNumber - 1].Name;
             ticket.FilmPrice = films.filmList[selectedFilmNumber - 1].Price;
             ticket.DateTime = DateTime.Now;
-
-
-            //ticketId++;
-            //ticket.Id = ticketId;
-            //ticket.FirstName = firstName;
-            //ticket.LastName = lastName;
-            //ticket.FilmName = films.filmList[selectedFilmNumber - 1].Name;
-            //ticket.FilmPrice = films.filmList[selectedFilmNumber - 1].Price;
-            //ticket.DateTime = DateTime.Now;
 
             tickets.TicketList.Add(new Ticket() { Id = ticket.Id, FirstName = ticket.FirstName, LastName = ticket.LastName, FilmName = ticket.FilmName, FilmPrice = ticket.FilmPrice, DateTime = ticket.DateTime, SeatNo = seats.SeatList[selectedSeatNumber - 1].Id });
             Console.Clear();
--- CinemaProject/ConsoleUI/Program.cs
+++ CinemaProject/ConsoleUI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using CinemaProject.Business;
 using CinemaProject.Business.ValidationRules;
 using CinemaProject.DataAccess;
@@ -12,75 +13,93 @@ namespace CinemaProject.ConsoleUI
         static void Main(string[] args)
         {
 
-
-              Start();
+            Start();
             Console.ReadKey();
-        
-    
+
+
         }
 
-    private static void Start()
-    {
-        List<User> users = new List<User>();
-        UserData userData = new UserData(users);
-        UserLoginOperation userLogin = new UserLoginOperation();
-
-
-
-        UserRegisterOperation userRegisterOperation = new UserRegisterOperation();
-        userRegisterOperation.Add(users);
-
-        int activeUserId = userLogin.Login(userData.UserDataGet());
-        UserLoginOperation userLoginOperation = new UserLoginOperation();
-
-        var seatsCollectiveList = new List<Seats>();
-        var seatsCollective = new SeatsCollective();
-        seatsCollective.Show(seatsCollectiveList);
-
-
-        var ticketList = new List<Ticket>();
-        //Data klasöründeki instancelar
-        var films = new Films();
-        var categories = new Categories();
-        var tickets = new Tickets(ticketList);
-
-        //Entity clasöründeki instancelar
-        var ticket = new Ticket();
-
-
-        //Operation klasöründeki instancelar
-        var filmInVision = new FilmInVision();
-        var filmlistByCategory = new FilmlistByCategory();
-        var chooseSeatNumber = new ChooseSeatNumber();
-        var ticketRegister = new TicketRegister();
-        var ticketInfo = new TicketInfo();
-
-        //Bu kısımda operasyonları sırasıyla çalışıyor ve işlem sona erdilmediği sürece kendini tekrar ediyor
-        while (true)
+        private static void Start()
         {
-
-            byte selectedCategoryNumber = filmInVision.Show(films, categories);
-            byte selectedFilmNumber = filmlistByCategory.Show(selectedCategoryNumber, films, categories);
-
-            byte selectedSeatNumber = chooseSeatNumber.Show(seatsCollectiveList[selectedFilmNumber - 1].SeatListGive());
+            List<User> users = new List<User>();
+            UserData userData = new UserData(users);
+            UserLoginOperation userLogin = new UserLoginOperation();
 
 
 
-            int ticketId = 0;
-            ticketRegister.Add(selectedSeatNumber, selectedFilmNumber, films, seatsCollectiveList[selectedFilmNumber - 1], ticket, tickets, ref ticketId, activeUserId, userData);
-
-            byte response = ticketInfo.Show(tickets);
-            if (response == 1)
+            Console.WriteLine("1 Kayıt Ol");
+            Console.WriteLine("2 Giriş Yap");
+            short num = Int16.Parse(Console.ReadLine());
+            UserRegisterOperation userRegisterOperation;
+            UserLoginOperation userLoginOperation;
+            int activeUserId = default;
+            if (num == 1)
             {
-                continue;
+                userRegisterOperation = new UserRegisterOperation();
+                userRegisterOperation.Add(users);
+
+                activeUserId = userLogin.Login(userData.UserDataGet());
+                userLoginOperation = new UserLoginOperation();
             }
             else
             {
-                break;
+                activeUserId = userLogin.Login(userData.UserDataGet());
+                userLoginOperation = new UserLoginOperation();
             }
-        }
 
+
+
+
+
+
+            var seatsCollectiveList = new List<Seats>();
+            var seatsCollective = new SeatsCollective();
+            seatsCollective.Show(seatsCollectiveList);
+
+
+            var ticketList = new List<Ticket>();
+            //Data klasöründeki instancelar
+            var films = new Films();
+            var categories = new Categories();
+            var tickets = new Tickets(ticketList);
+
+            //Entity clasöründeki instancelar
+            var ticket = new Ticket();
+
+
+            //Operation klasöründeki instancelar
+            var filmInVision = new FilmInVision();
+            var filmlistByCategory = new FilmlistByCategory();
+            var chooseSeatNumber = new ChooseSeatNumber();
+            var ticketRegister = new TicketRegister();
+            var ticketInfo = new TicketInfo();
+
+            //Bu kısımda operasyonları sırasıyla çalışıyor ve işlem sona erdilmediği sürece kendini tekrar ediyor
+            while (true)
+            {
+
+                byte selectedCategoryNumber = filmInVision.Show(films, categories);
+                byte selectedFilmNumber = filmlistByCategory.Show(selectedCategoryNumber, films, categories);
+
+                byte selectedSeatNumber = chooseSeatNumber.Show(seatsCollectiveList[selectedFilmNumber - 1].SeatListGive());
+
+
+
+                int ticketId = 0;
+                ticketRegister.Add(selectedSeatNumber, selectedFilmNumber, films, seatsCollectiveList[selectedFilmNumber - 1], ticket, tickets, ref ticketId, activeUserId, userData);
+
+                byte response = ticketInfo.Show(tickets);
+                if (response == 1)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+        }
     }
-}
 
 }
