fe4f0df5bb131223646e4cb24766d3cb996215af
ahmetozdemr <ahmetzdmiir@gmail.com>
2023-03-26 19:27:30 +0300

    Refactoring

---
+++ CinemaProject/Business/Abstract/IFilmService.cs
@@ -0,0 +1,14 @@
+﻿using CinemaProject.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaProject.Business.Abstract
+{
+    public interface IFilmService
+    {
+        List<Film> GetAll();
+    }
+}
